b2134fdc6ed1f4e2ff29cabf014fa350f026acc6
omprakash panda <ommprakashp78@gmail.com>
2026-08-11 20:12:32 +0530

    my first query

---
+++ classroom.sql
@@ -0,0 +1,8 @@
+
+-- This is my first SQL file. It contains the schema for a simple classroom database.
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+);
